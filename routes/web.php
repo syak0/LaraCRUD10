ffd3ea9c3a93c3175b78c782b7f3456632c79a27
syak0 <syakirizidan1009@gmail.com>
2023-07-16 17:25:18 +0700

    < sedikit perbaikan >

--- routes/web.php
+++ routes/web.php
@@ -4,33 +4,19 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyCRUDController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::view('/', 'auth.login');
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-Route::get('./', function () {
-    return view('companies', CompanyCRUDController::class);
-});
-
-Route::resource('companies', CompanyCRUDController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('./', function () {
+        return view('companies', CompanyCRUDController::class);
+    }); 
+    Route::resource('companies', CompanyCRUDController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
